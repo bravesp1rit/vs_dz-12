75a9c55d5380e2302f8296815dd17815f64aae64
brave <progpavlovmark@gmail.com>
2023-02-03 17:24:42 +0200

    dz-12 1.1

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
 'use strict';
 
 const userYearOfBirth = prompt('Введи свой год рождения:', '');
+console.log(userYearOfBirth);
+if (userYearOfBirth !== null && !isNaN(+userYearOfBirth) && userYearOfBirth != '')
 
-if (userYearOfBirth == null || !isNaN(+userYearOfBirth)){
+    let userAge = new Date().getFullYear() - userYearOfBirth;
+
+    const userCity = prompt('Введи город в котором ты живешь:', '');
+    if (userCity !== null && userCity.trim().length){
+
+        let userCityResult = null;
+        if (userCity == 'Киев'){
+            userCityResult = 'Ты живешь в столице Украины';
+        } else if (userCity == 'Берлин'){
+            userCityResult = 'Ты живешь в столице Германии';
+        } else if (userCity == 'Париж'){
+            userCityResult = 'Ты живешь в столице Франции';
+        } else if (userCity == ''){
+            userCityResult = 'Где ты вообще живёшь';
+        } else {
+            userCityResult = `Ты живешь в городе ${userCity}`;
+        }
+
+        const userFavouriteSport = prompt('Введи свой любимый спорт:', '');
+        if (userFavouriteSport !== null && userFavouriteSport.trim().length){
+            let userFavouriteSportResult = null;
+            if (userFavouriteSport == 'Футбол'){
+                userFavouriteSportResult = 'Круто! Хочешь стать как Месси';
+            } else if (userFavouriteSport == 'Баскетбол'){
+                userFavouriteSportResult = 'Круто! Хочешь стать как Джордан';
+            } else if (userFavouriteSport == 'Теннис'){
+                userFavouriteSportResult = 'Круто! Хочешь стать как Надаь';
+            } else {
+                userFavouriteSportResult = 'Интересно! Не слышал о таком виде спорта';
+            }
+            alert('Твой возраст: ' + userAge + '\n' + userCityResult + '\n' + userFavouriteSportResult );
+        } else {
+            alert('Жаль, что Вы не захотели ввести свой любимый спорт');
+        }
+    } else{
+        alert('Жаль, что Вы не захотели ввести свой город');
+    }
+} else{
     alert('Жаль, что Вы не захотели ввести свой год рождения');
-}
-let userAge = new Date().getFullYear() - userYearOfBirth;
-
-const userCity = prompt('Введи город в котором ты живешь:', '');
-
-if (userCity == null){
-    alert('Жаль, что Вы не захотели ввести свой город');
-}
-
-let userCityResult = null;
-if (userCity == 'Киев'){
-    userCityResult = 'Ты живешь в столице Украины';
-} else if (userCity == 'Берлин'){
-    userCityResult = 'Ты живешь в столице Германии';
-} else if (userCity == 'Париж'){
-    userCityResult = 'Ты живешь в столице Франции';
-} else if (userCity == ''){
-    userCityResult = 'Где ты вообще живёшь';
-} else {
-    userCityResult = `Ты живешь в городе ${userCity}`;
-}
-const userFavouriteSport = prompt('Введи свой любимый спорт:', '');
-let userFavouriteSportResult = null;
-if (userFavouriteSport == 'Футбол'){
-    userFavouriteSportResult = 'Круто! Хочешь стать как Месси';
-} else if (userFavouriteSport == 'Баскетбол'){
-    userFavouriteSportResult = 'Круто! Хочешь стать как Джордан';
-} else if (userFavouriteSport == 'Теннис'){
-    userFavouriteSportResult = 'Круто! Хочешь стать как Надаь';
-} else {
-    userFavouriteSportResult = 'Интересно! Не слышал о таком виде спорта';
-}
-
-if (Number.isNaN(userFavouriteSport) || userFavouriteSport == null){
-    alert('Жаль, что Вы не захотели ввести свой любимый спорт');
-} else {
-    alert('Твой возраст: ' + userAge + '\n' + userCityResult + '\n' + userFavouriteSportResult );
 }
